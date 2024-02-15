a2d656e9c56e2b8838e9cdd364537d7f50045974
Georgi Yunchov <georgiiunchov@gmail.com>
2024-02-15 14:57:27 +0200

    Add edit functionality.

--- React/React-Essentials-Deep-Dive/client/src/components/Player.jsx
+++ React/React-Essentials-Deep-Dive/client/src/components/Player.jsx
@@ -1,11 +1,34 @@
+import { useState } from 'react';
+
 export default function Player({ name, symbol }) {
+    const [playerName, setPlayerName] = useState(name);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleEditClick = () => setIsEditing((state) => !state);
+
+    const handleChange = (e) => {
+        setPlayerName(e.target.value);
+    };
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {!isEditing && (
+                    <span className="player-name">{playerName}</span>
+                )}
+                {isEditing && (
+                    <input
+                        type="text"
+                        required
+                        value={playerName}
+                        onChange={handleChange}
+                    />
+                )}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>
+                {isEditing ? 'Save' : 'Edit'}
+            </button>
         </li>
     );
 }
