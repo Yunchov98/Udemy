8fe7b9b24a830d676e5ad5ad7c28cbffaff81765
Georgi Yunchov <georgiiunchov@gmail.com>
2024-04-01 19:18:56 +0300

    Create timer challenge component.

---
+++ React/Working-With-Refs-And-Portals/client/src/components/TimerChallenge.jsx
@@ -0,0 +1,38 @@
+import { useRef, useState } from 'react';
+
+export default function TimerChallenge({ title, targetTime }) {
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
+
+    const timer = useRef();
+
+    const handleStart = () => {
+        setTimerStarted(() => true);
+
+        timer.current = setTimeout(() => {
+            setTimerExpired(() => true);
+        }, targetTime * 1000);
+    };
+
+    const handleStop = () => {
+        clearTimeout(timer.current);
+    };
+
+    return (
+        <section className="challenge">
+            <h2>{title}</h2>
+            {timerExpired && <p>You lost!</p>}
+            <p className="challenge-time">
+                {targetTime} second{targetTime > 1 ? 's' : ''}
+            </p>
+            <p>
+                <button onClick={timerStarted ? handleStop : handleStart}>
+                    {timerStarted ? 'Stop' : 'Start'} Challenge
+                </button>
+            </p>
+            <p className={timerStarted ? 'active' : ''}>
+                Time is runing.../Timer inactive
+            </p>
+        </section>
+    );
+}
