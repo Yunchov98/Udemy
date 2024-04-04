f31f2ff130990368aedff6fff1c707f52615b16f
Georgi Yunchov <georgiiunchov@gmail.com>
2024-04-04 17:07:06 +0300

    Add state object for the projects and hadnle add project function.

--- React/Project-Management-App-Practice-Project/client/src/App.jsx
+++ React/Project-Management-App-Practice-Project/client/src/App.jsx
@@ -1,11 +1,32 @@
+import { useState } from 'react';
+
 import NoProjectSelected from './components/NoProjectSelected';
 import ProjectsSidebar from './components/ProjectsSidebar';
+import NewProject from './components/NewProject';
 
 function App() {
+    const [projectsState, setProjectsState] = useState({
+        selectedProject: undefined,
+        projects: [],
+    });
+
+    const handleStartAddProject = () => {
+        setProjectsState((state) => {
+            return {
+                ...state,
+                selectedProject: null,
+            };
+        });
+    };
+
     return (
         <main className="h-screen my-8 flex gap-8">
-            <ProjectsSidebar />
-            <NoProjectSelected />
+            <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+            {projectsState.selectedProject === null ? (
+                <NewProject />
+            ) : (
+                <NoProjectSelected onStartAddProject={handleStartAddProject} />
+            )}
         </main>
     );
 }
