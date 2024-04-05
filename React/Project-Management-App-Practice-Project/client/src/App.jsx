5d8b0fa2f0181ed80677855bf8b246072ed93ddb
Georgi Yunchov <georgiiunchov@gmail.com>
2024-04-05 11:46:48 +0300

    Add select project functionality. Render the content conditionally.

--- React/Project-Management-App-Practice-Project/client/src/App.jsx
+++ React/Project-Management-App-Practice-Project/client/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import NoProjectSelected from './components/NoProjectSelected';
 import ProjectsSidebar from './components/ProjectsSidebar';
 import NewProject from './components/NewProject';
+import SelectedProject from './components/SelectedProject';
 
 function App() {
     const [projectsState, setProjectsState] = useState({
@@ -43,20 +44,42 @@ function App() {
         });
     };
 
+    const handleSelectProject = (id) => {
+        setProjectsState((state) => {
+            return {
+                ...state,
+                selectedProject: id,
+            };
+        });
+    };
+
+    const selectedProject = projectsState.projects.find(
+        (project) => project.id === projectsState.selectedProject
+    );
+
+    let content = <SelectedProject project={selectedProject} />;
+
+    if (projectsState.selectedProject === null) {
+        content = (
+            <NewProject
+                onAdd={handleAddProject}
+                onCancel={handleCancelAddProject}
+            />
+        );
+    } else if (projectsState.selectedProject === undefined) {
+        content = (
+            <NoProjectSelected onStartAddProject={handleStartAddProject} />
+        );
+    }
+
     return (
         <main className="h-screen my-8 flex gap-8">
             <ProjectsSidebar
                 onStartAddProject={handleStartAddProject}
                 projects={projectsState.projects}
+                onSelectProject={handleSelectProject}
             />
-            {projectsState.selectedProject === null ? (
-                <NewProject
-                    onAdd={handleAddProject}
-                    onCancel={handleCancelAddProject}
-                />
-            ) : (
-                <NoProjectSelected onStartAddProject={handleStartAddProject} />
-            )}
+            {content}
         </main>
     );
 }
