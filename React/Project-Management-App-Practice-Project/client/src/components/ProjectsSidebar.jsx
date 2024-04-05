d32d5fd8a33e275e6aa5c4ee55b50f6c2dcc49d6
Georgi Yunchov <georgiiunchov@gmail.com>
2024-04-05 12:45:42 +0300

    Edit cssClasses string.

--- React/Project-Management-App-Practice-Project/client/src/components/ProjectsSidebar.jsx
+++ React/Project-Management-App-Practice-Project/client/src/components/ProjectsSidebar.jsx
@@ -20,7 +20,7 @@ export default function ProjectsSidebar({
                         'w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800';
 
                     if (project.id === selectedProjectId) {
-                        cssClasses += ' bg-stone-800, text-stone-200';
+                        cssClasses += ' bg-stone-800 text-stone-200';
                     } else {
                         cssClasses += ' text-stone-400';
                     }
