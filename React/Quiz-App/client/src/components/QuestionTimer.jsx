57b434bdcc269d1a58414dbba7188773877c5b01
Yunchov98 <georgiiunchov@gmail.com>
2024-04-29 18:36:28 +0300

    Add id and class to the progress element.

--- React/Quiz-App/client/src/components/QuestionTimer.jsx
+++ React/Quiz-App/client/src/components/QuestionTimer.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-export default function QuestionTimer({ timeout, onTimeout }) {
+export default function QuestionTimer({ timeout, onTimeout, mode }) {
     const [remainingTime, setRemainingTime] = useState(timeout);
 
     useEffect(() => {
@@ -24,5 +24,12 @@ export default function QuestionTimer({ timeout, onTimeout }) {
         };
     }, []);
 
-    return <progress value={remainingTime} max={timeout} />;
+    return (
+        <progress
+            id="question-time"
+            className={mode}
+            value={remainingTime}
+            max={timeout}
+        />
+    );
 }
