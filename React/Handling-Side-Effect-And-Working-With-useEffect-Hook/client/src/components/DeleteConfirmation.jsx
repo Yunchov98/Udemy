2a2e3cc8795a60158b926bad243d2f946c6c7b24
Yunchov98 <georgiiunchov@gmail.com>
2024-04-29 13:42:21 +0300

    Add progress bar for the place deletion. Add state for the remaining time and interval for the progress bar.

--- React/Handling-Side-Effect-And-Working-With-useEffect-Hook/client/src/components/DeleteConfirmation.jsx
+++ React/Handling-Side-Effect-And-Working-With-useEffect-Hook/client/src/components/DeleteConfirmation.jsx
@@ -1,11 +1,24 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+    const [remainingTime, setRemainingTime] = useState(TIMER);
+
     useEffect(() => {
-        console.log('timer is on');
+        const interval = setInterval(() => {
+            setRemainingTime((prevTime) => prevTime - 10);
+        }, 10);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    useEffect(() => {
         const timer = setTimeout(() => {
             onConfirm();
-        }, 3000);
+        }, TIMER);
 
         return () => {
             clearTimeout(timer);
@@ -24,6 +37,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
                     Yes
                 </button>
             </div>
+            <progress value={remainingTime} max={TIMER} />
         </div>
     );
 }
