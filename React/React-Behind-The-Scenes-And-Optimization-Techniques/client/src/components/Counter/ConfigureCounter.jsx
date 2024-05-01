2a3b9b7156c7aa8b0a6c6419fb60fb60d8ef383d
Yunchov98 <georgiiunchov@gmail.com>
2024-05-01 17:06:42 +0300

    Add configure counter component to avoid rerendering.

---
+++ React/React-Behind-The-Scenes-And-Optimization-Techniques/client/src/components/Counter/ConfigureCounter.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { log } from '../../log';
+
+ConfigureCounter.propTypes = {
+    onSet: PropTypes.func,
+};
+
+export default function ConfigureCounter({ onSet }) {
+    log('<ConfigureCounter />', 1)
+    const [enteredNumber, setEnteredNumber] = useState(0);
+
+    function handleChange(event) {
+        setEnteredNumber(+event.target.value);
+    }
+
+    function handleSetClick() {
+        onSet(enteredNumber);
+        setEnteredNumber(0);
+    }
+
+    return (
+        <section id="configure-counter">
+            <h2>Set Counter</h2>
+            <input
+                type="number"
+                onChange={handleChange}
+                value={enteredNumber}
+            />
+            <button onClick={handleSetClick}>Set</button>
+        </section>
+    );
+}
