7a3a5247996fffbdbdc1b64000e72c717cd2a101
Yunchov98 <georgiiunchov@gmail.com>
2024-07-07 15:40:37 +0300

    Add state for fetching

--- React/Sending-Http-Requests/client/src/components/AvailablePlaces.jsx
+++ React/Sending-Http-Requests/client/src/components/AvailablePlaces.jsx
@@ -3,18 +3,26 @@ import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+    const [isFetching, setIsFetching] = useState(false);
     const [availablePlaces, setAvailablePlaces] = useState([]);
 
     useEffect(() => {
+        setIsFetching(true);
+
         fetch('http://localhost:3000/places')
             .then((response) => response.json())
-            .then((resData) => setAvailablePlaces(resData.places));
+            .then((resData) => {
+                setAvailablePlaces(resData.places);
+                setIsFetching(false);
+            });
     }, []);
 
     return (
         <Places
             title="Available Places"
             places={availablePlaces}
+            isLoading={isFetching}
+            loadingText="Fetching place data..."
             fallbackText="No places available."
             onSelectPlace={onSelectPlace}
         />
