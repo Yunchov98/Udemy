6f792fcd87753d3984d83ffc7dbd8e3f54cd064d
Yunchov98 <georgiiunchov@gmail.com>
2024-07-10 19:39:58 +0300

    Outsorce validation logic

--- React/Working-With-Forms-And-User-Input/client/src/components/StateLogin.jsx
+++ React/Working-With-Forms-And-User-Input/client/src/components/StateLogin.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+
+import { isEmail, isNotEmpty, hasMinLength } from '../util/validation.js';
 import Input from './Input';
 
 export default function Login() {
@@ -12,8 +14,10 @@ export default function Login() {
         password: false,
     });
 
-    const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
-    const passwordIsInvalid = didEdit.password && enteredValues.password.trim().length < 6;
+    const emailIsInvalid =
+        didEdit.email && !isEmail(enteredValues.email) && !isNotEmpty;
+    const passwordIsInvalid =
+        didEdit.password && !hasMinLength(enteredValues.password, 6);
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -76,7 +80,9 @@ export default function Login() {
                         handleInputChange('password', event.target.value)
                     }
                     value={enteredValues.password}
-                    error={passwordIsInvalid && 'Please enter a valid password.'}
+                    error={
+                        passwordIsInvalid && 'Please enter a valid password.'
+                    }
                 />
             </div>
 
