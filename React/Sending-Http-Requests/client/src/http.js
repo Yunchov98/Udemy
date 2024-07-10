5de5269ca1b26e86c405d383643dbe4c6f0e7819
Yunchov98 <georgiiunchov@gmail.com>
2024-07-10 12:15:43 +0300

    Add put request to update places

--- React/Sending-Http-Requests/client/src/http.js
+++ React/Sending-Http-Requests/client/src/http.js
@@ -2,10 +2,28 @@ export async function fetchAvailablePlaces() {
     const response = await fetch('http://localhost:3000/places');
 
     if (!response.ok) {
-        throw new Error('Failed to fetch places!');
+        throw new Error('Failed to fetch places.');
     }
 
     const resData = await response.json();
 
     return resData.places;
+}
+
+export async function updateUserPlaces(places) {
+    const response = await fetch('http://localhost:3000/user-places', {
+        method: 'PUT',
+        body: JSON.stringify({ places }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to update user places.');
+    }
+
+    const resData = await response.json();
+
+    return resData.message;
 }
