1c83598d82c55fe3b61dfcb0d00b154f559eec19
Yunchov98 <georgiiunchov@gmail.com>
2024-07-10 12:26:12 +0300

    Add error handling

--- React/Sending-Http-Requests/client/src/App.jsx
+++ React/Sending-Http-Requests/client/src/App.jsx
@@ -6,11 +6,14 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
 import { updateUserPlaces } from './http.js';
+import Error from './components/Error.jsx';
 
 function App() {
     const selectedPlace = useRef();
 
     const [userPlaces, setUserPlaces] = useState([]);
+
+    const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
 
     const [modalIsOpen, setModalIsOpen] = useState(false);
 
@@ -39,7 +42,10 @@ function App() {
         try {
             await updateUserPlaces([selectedPlace, ...userPlaces]);
         } catch (error) {
-            console.log(error);
+            setUserPlaces(userPlaces);
+            setErrorUpdatingPlaces({
+                message: error.message || 'Failed to update places.',
+            });
         }
     }
 
@@ -53,8 +59,21 @@ function App() {
         setModalIsOpen(false);
     }, []);
 
+    function handleError() {
+        setErrorUpdatingPlaces(null);
+    }
+
     return (
         <>
+            <Modal open={errorUpdatingPlaces} onClose={handleError}>
+                {errorUpdatingPlaces && (
+                    <Error
+                        title={'An error occurred!'}
+                        message={errorUpdatingPlaces.message}
+                        onConfirm={handleError}
+                    />
+                )}
+            </Modal>
             <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
                 <DeleteConfirmation
                     onCancel={handleStopRemovePlace}
