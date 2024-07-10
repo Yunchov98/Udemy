a6368572887b63aa8d29e705cf55a74a8ef0262d
Yunchov98 <georgiiunchov@gmail.com>
2024-07-10 11:21:15 +0300

    Add error handling

--- React/Sending-Http-Requests/client/src/components/AvailablePlaces.jsx
+++ React/Sending-Http-Requests/client/src/components/AvailablePlaces.jsx
@@ -1,21 +1,40 @@
 import { useEffect, useState } from 'react';
 
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
     const [isFetching, setIsFetching] = useState(false);
     const [availablePlaces, setAvailablePlaces] = useState([]);
+    const [error, setError] = useState();
 
     useEffect(() => {
         setIsFetching(true);
 
         fetch('http://localhost:3000/places')
-            .then((response) => response.json())
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Failed to fetch places!');
+                }
+
+                return response.json();
+            })
             .then((resData) => {
                 setAvailablePlaces(resData.places);
                 setIsFetching(false);
-            });
+            })
+            .catch((error) =>
+                setError({
+                    message:
+                        error.message ||
+                        'Could not fetch places, please try again later!',
+                })
+            );
     }, []);
+
+    if (error) {
+        return <Error title={'An error occurred!'} message={error.message} />;
+    }
 
     return (
         <Places
