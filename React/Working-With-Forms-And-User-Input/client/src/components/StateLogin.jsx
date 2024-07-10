95544dee080fe0ca35c10287041340948401e97e
Yunchov98 <georgiiunchov@gmail.com>
2024-07-10 19:19:23 +0300

    Add form validation

--- React/Working-With-Forms-And-User-Input/client/src/components/StateLogin.jsx
+++ React/Working-With-Forms-And-User-Input/client/src/components/StateLogin.jsx
@@ -6,8 +6,24 @@ export default function Login() {
         password: '',
     });
 
+    const [didEdit, setDidEdit] = useState({
+        email: false,
+        password: false,
+    });
+
+    const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
+
     function handleSubmit(event) {
         event.preventDefault();
+
+        if (
+            enteredValues.email.length === 0 &&
+            enteredValues.password.length === 0
+        ) {
+            console.log('Error');
+            return;
+        }
+
         console.log(enteredValues);
     }
 
@@ -15,6 +31,18 @@ export default function Login() {
         setEnteredValues((prevValues) => ({
             ...prevValues,
             [identifier]: value,
+        }));
+
+        setDidEdit((prevEdit) => ({
+            ...prevEdit,
+            [identifier]: false,
+        }));
+    }
+
+    function handleInputBlur(identifier) {
+        setDidEdit((prevEdit) => ({
+            ...prevEdit,
+            [identifier]: true,
         }));
     }
 
@@ -29,11 +57,17 @@ export default function Login() {
                         id="email"
                         type="email"
                         name="email"
+                        onBlur={() => handleInputBlur('email')}
                         onChange={(event) =>
                             handleInputChange('email', event.target.value)
                         }
                         value={enteredValues.email}
                     />
+                    <div className="control-error">
+                        {emailIsInvalid && (
+                            <p>Please enter a valid email address.</p>
+                        )}
+                    </div>
                 </div>
 
                 <div className="control no-margin">
@@ -42,6 +76,7 @@ export default function Login() {
                         id="password"
                         type="password"
                         name="password"
+                        onBlur={() => handleInputBlur('password')}
                         onChange={(event) =>
                             handleInputChange('password', event.target.value)
                         }
