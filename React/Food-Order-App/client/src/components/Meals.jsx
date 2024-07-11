f9744cd6a3a812f3f2de016c910fa310183fd159
Yunchov98 <georgiiunchov@gmail.com>
2024-07-11 08:38:28 +0300

    Create meals component

---
+++ React/Food-Order-App/client/src/components/Meals.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+import MealItem from './MealItem';
+
+export default function Meals() {
+    const [loadedMeals, setLoadedMeals] = useState([]);
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch('http://localhost:3000/meals');
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch. Please try again alter.');
+            }
+
+            const meals = await response.json();
+
+            setLoadedMeals(meals);
+        };
+
+        fetchMeals();
+    }, []);
+
+    return (
+        <ul id="meals">
+            {loadedMeals.map((meal) => (
+                <MealItem key={meal.id} meal={meal} />
+            ))}
+        </ul>
+    );
+}
