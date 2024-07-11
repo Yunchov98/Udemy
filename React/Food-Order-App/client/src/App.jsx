08e49a14d6e89266f52412ea7d3a01adb8a5a72f
Yunchov98 <georgiiunchov@gmail.com>
2024-07-11 10:40:46 +0300

    Use cart context provider

--- React/Food-Order-App/client/src/App.jsx
+++ React/Food-Order-App/client/src/App.jsx
@@ -1,11 +1,15 @@
-import Header from "./components/Header";
-import Meals from "./components/Meals";
+import { CartContextProvider } from './store/cartContext';
+
+import Header from './components/Header';
+import Meals from './components/Meals';
 
 function App() {
     return (
         <>
-        <Header />
-        <Meals />
+            <CartContextProvider>
+                <Header />
+                <Meals />
+            </CartContextProvider>
         </>
     );
 }
