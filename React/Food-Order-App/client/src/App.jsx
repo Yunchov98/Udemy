4b9dcf2760417cdf404240313d76b19872ee6e98
Yunchov98 <georgiiunchov@gmail.com>
2024-07-11 12:05:11 +0300

    Add checkout component

--- React/Food-Order-App/client/src/App.jsx
+++ React/Food-Order-App/client/src/App.jsx
@@ -5,6 +5,7 @@ import { UserProgressContextProvider } from './store/userProgressContext';
 import Header from './components/Header';
 import Meals from './components/Meals';
 import Cart from './components/Cart';
+import Checkout from './components/Checkout';
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                     <Header />
                     <Meals />
                     <Cart />
+                    <Checkout />
                 </CartContextProvider>
             </UserProgressContextProvider>
         </>
