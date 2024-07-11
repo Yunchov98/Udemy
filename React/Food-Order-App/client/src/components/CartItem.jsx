4acf6715e7d8370f5510c8c9a623e303dad2055c
Yunchov98 <georgiiunchov@gmail.com>
2024-07-11 11:30:43 +0300

    Create cart item component

---
+++ React/Food-Order-App/client/src/components/CartItem.jsx
@@ -0,0 +1,22 @@
+import { currencyForamtter } from '../util/foramtting';
+
+export default function CartItem({
+    name,
+    quantity,
+    price,
+    onIncrease,
+    onDecrease,
+}) {
+    return (
+        <li className="cart-item">
+            <p>
+                {name} - {quantity} x {currencyForamtter.format(price)}
+            </p>
+            <p className="cart-item-actions">
+                <button onClick={onDecrease}>-</button>
+                <span>{quantity}</span>
+                <button onClick={onIncrease}>+</button>
+            </p>
+        </li>
+    );
+}
