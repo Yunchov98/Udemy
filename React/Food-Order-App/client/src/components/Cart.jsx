bd052d6b03d339451aa0af2a4b9f2f3772320ed9
Yunchov98 <georgiiunchov@gmail.com>
2024-07-11 11:30:59 +0300

    Add cart item component

--- React/Food-Order-App/client/src/components/Cart.jsx
+++ React/Food-Order-App/client/src/components/Cart.jsx
@@ -6,9 +6,10 @@ import CartContext from '../store/cartContext';
 import Modal from './UI/Modal';
 import Button from './UI/Button';
 import UserProgressContext from '../store/userProgressContext';
+import CartItem from './CartItem';
 
 export default function Cart() {
-    const { items } = useContext(CartContext);
+    const { items, addItem, removeItem } = useContext(CartContext);
     const { progress, hideCart } = useContext(UserProgressContext);
 
     const cartTotal = items.reduce((totalPrice, item) => {
@@ -24,9 +25,14 @@ export default function Cart() {
             <h2>Your Cart</h2>
             <ul>
                 {items.map((item) => (
-                    <li key={item.id}>
-                        {item.name} - {item.quantity}
-                    </li>
+                    <CartItem
+                        key={item.id}
+                        name={item.name}
+                        price={item.price}
+                        quantity={item.quantity}
+                        onIncrease={() => addItem(item)}
+                        onDecrease={() => removeItem(item.id)}
+                    />
                 ))}
             </ul>
             <p className="cart-total">{currencyForamtter.format(cartTotal)}</p>
