47212f42fe45fe600b703e8829a42ae8c969b85d
Yunchov98 <georgiiunchov@gmail.com>
2024-07-12 08:28:17 +0300

    throw an error if the response is not ok instead of return it

--- React/Food-Order-App/client/src/hooks/useHttp.js
+++ React/Food-Order-App/client/src/hooks/useHttp.js
@@ -6,7 +6,7 @@ async function sendHttpRequest(url, config) {
     const resData = await response.json();
 
     if (!response.ok) {
-        return (
+        throw new Error(
             resData.message || 'Something went wrong, failed to send request.'
         );
     }
