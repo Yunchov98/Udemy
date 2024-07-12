c7d7c7c978d3d5e91aa77c8d1b9daff276b263e0
Yunchov98 <georgiiunchov@gmail.com>
2024-07-12 07:40:24 +0300

    Send post request with order data

--- React/Food-Order-App/client/src/components/Checkout.jsx
+++ React/Food-Order-App/client/src/components/Checkout.jsx
@@ -19,13 +19,33 @@ export default function Checkout() {
         hideCheckout();
     }
 
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const fd = new FormData(event.target);
+        const customerData = Object.fromEntries(fd.entries());
+
+        fetch('http://localhost:3000/orders', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                order: {
+                    items,
+                    customer: customerData,
+                },
+            }),
+        });
+    }
+
     return (
         <Modal open={progress === 'checkout'} onClose={handleClose}>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>Checkout</h2>
                 <p>Total Amount: {currencyForamtter.format(cartTotal)}</p>
 
-                <Input label="Full Name" type="text" id="full-name" />
+                <Input label="Full Name" type="text" id="name" />
                 <Input label="Email Address" type="email" id="email" />
                 <Input label="Street" type="text" id="street" />
                 <div className="control-row">
