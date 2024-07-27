56fb230be0d2bafe25867de1c87d411e4305b71e
Yunchov98 <georgiiunchov@gmail.com>
2024-07-27 17:41:01 +0300

    Create toggle function for the cart component

--- React/Advanced-Redux/client/src/components/Cart/CartButton.js
+++ React/Advanced-Redux/client/src/components/Cart/CartButton.js
@@ -1,10 +1,21 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { uiActions } from '../../store/ui-slice';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+    const dispatch = useDispatch();
+
+    const cartQuantity = useSelector((state) => state.cart.totalQuantity);
+
+    const toggleCartHandle = () => {
+        dispatch(uiActions.toggle());
+    };
+
     return (
-        <button className={classes.button}>
+        <button className={classes.button} onClick={toggleCartHandle}>
             <span>My Cart</span>
-            <span className={classes.badge}>1</span>
+            <span className={classes.badge}>{cartQuantity}</span>
         </button>
     );
 };
