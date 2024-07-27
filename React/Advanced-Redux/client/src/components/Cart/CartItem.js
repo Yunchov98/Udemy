7d74b709386d467b2f9d44d3c299c428d7f0abba
Yunchov98 <georgiiunchov@gmail.com>
2024-07-27 17:41:29 +0300

    Add functionliaty to the plus and minus buttons

--- React/Advanced-Redux/client/src/components/Cart/CartItem.js
+++ React/Advanced-Redux/client/src/components/Cart/CartItem.js
@@ -1,7 +1,27 @@
+import { useDispatch } from 'react-redux';
+
+import { cartActions } from '../../store/cart-slice';
+
 import classes from './CartItem.module.css';
 
 const CartItem = (props) => {
-    const { title, quantity, total, price } = props.item;
+    const { title, quantity, total, price, id } = props.item;
+
+    const dispatch = useDispatch();
+
+    const addItemHandler = () => {
+        dispatch(
+            cartActions.addItemToCart({
+                id,
+                title,
+                price,
+            })
+        );
+    };
+
+    const removeItemHandler = () => {
+        dispatch(cartActions.removeItemFromCart(id));
+    };
 
     return (
         <li className={classes.item}>
@@ -19,8 +39,8 @@ const CartItem = (props) => {
                     x <span>{quantity}</span>
                 </div>
                 <div className={classes.actions}>
-                    <button>-</button>
-                    <button>+</button>
+                    <button onClick={removeItemHandler}>-</button>
+                    <button onClick={addItemHandler}>+</button>
                 </div>
             </div>
         </li>
