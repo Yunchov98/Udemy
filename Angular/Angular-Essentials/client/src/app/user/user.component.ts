12f0d07b2ab3511d819372f358f3bb831daf0c67
Yunchov98 <georgiiunchov@gmail.com>
2024-08-04 11:10:18 +0300

    remove signals

--- Angular/Angular-Essentials/client/src/app/user/user.component.ts
+++ Angular/Angular-Essentials/client/src/app/user/user.component.ts
@@ -1,8 +1,10 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -11,16 +13,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`);
+  @Input({ required: true }) user!: User;
+  @Output() select = new EventEmitter();
 
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar;
-  // }
+  get imagePath() {
+    return `assets/users/${this.user.avatar}`;
+  }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.select.emit(this.user.id);
   }
 }
