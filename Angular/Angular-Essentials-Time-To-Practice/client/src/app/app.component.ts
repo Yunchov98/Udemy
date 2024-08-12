69a462f205000fe18db426cc1dae902847dbbfc6
Yunchov98 <georgiiunchov@gmail.com>
2024-08-12 20:04:05 +0300

    Add investment input interface

--- Angular/Angular-Essentials-Time-To-Practice/client/src/app/app.component.ts
+++ Angular/Angular-Essentials-Time-To-Practice/client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
+import type { InvestmentInput } from './investment-input.module';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,7 @@ import { UserInputComponent } from './user-input/user-input.component';
   imports: [HeaderComponent, UserInputComponent],
 })
 export class AppComponent {
-  onCalculateInvestmentResults(data: {
-    initialInvestment: number;
-    annualInvestment: number;
-    expectedReturn: number;
-    duration: number;
-  }) {
+  onCalculateInvestmentResults(data: InvestmentInput) {
     const { annualInvestment, duration, expectedReturn, initialInvestment } =
       data;
     const annualData = [];
