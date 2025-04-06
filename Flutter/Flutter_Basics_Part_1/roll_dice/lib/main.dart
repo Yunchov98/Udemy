f4ae1e55f40c2bdd686f6436643546ba2c990d13
Yunchov98 <georgiiunchov@gmail.com>
2025-04-06 15:52:26 +0300

    Add gradient container widget

--- Flutter/Flutter_Basics_Part_1/roll_dice/lib/main.dart
+++ Flutter/Flutter_Basics_Part_1/roll_dice/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:roll_dice/gradient_container.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,14 +16,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
         appBar: AppBar(title: const Text('First App')),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text('Hello World!'),
-              Text('It\'s time to learn Flutter!'),
-            ],
-          ),
+        body: const GradientContainer(
+          Color.fromARGB(255, 26, 2, 80),
+          Color.fromARGB(255, 45, 7, 98),
         ),
       ),
     );
